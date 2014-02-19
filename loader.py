5941717e89ac59fb1ea5e0f9d28c8ef68ce688c8
fak <fkrueger@ebi.ac.uk>
2014-02-19 10:38:27 +0000

    removed mysql functions

--- loader.py
+++ loader.py
@@ -12,7 +12,6 @@ fkrueger@ebi.ac.uk
 import os
 import subprocess
 import sys
-import queryDevice
 import yaml
 import pgQuery
 
@@ -42,39 +41,6 @@ def readfile(path, key_name, val_name):
 
 
 
-def retrieve_acts(dom_string, params):
-    """Run a query for act_id, tid, component_id, compd_id and domain_name.
-
-    Inputs:
-    dom_string -- A string specifying the domain names eg. "7tm_1','Pkinase','Pkinase_tyr"
-    params -- dictionary holding details of the connection string
-
-    """
-    acts = queryDevice.queryDevice("""
-    SELECT DISTINCT act.activity_id, ass.tid, tc.component_id, cd.compd_id, dm.domain_name
-                      FROM activities act
-                      JOIN assays ass
-                          ON ass.assay_id = act.assay_id
-                      JOIN target_dictionary td
-                          ON ass.tid = td.tid
-                      JOIN target_components tc
-                          ON ass.tid = tc.tid
-                      JOIN component_domains cd
-                          ON tc.component_id = cd.component_id
-                      JOIN domains dm
-                          ON dm.domain_id = cd.domain_id
-                     WHERE ass.assay_type IN('B','F')
-                     AND td.target_type IN('PROTEIN COMPLEX', 'SINGLE PROTEIN')
-                     AND act.standard_relation ='='
-                     AND ass.relationship_type = 'D'
-                     AND act.standard_type IN(
-                       'Ki', 'Kd', 'IC50', 'EC50', 'AC50',
-                       'log Ki', 'log Kd', 'log IC50', 'Log EC50', 'Log AC50'
-                       'pKi', 'pKd', 'pIC50', 'pEC50', 'pAC50'
-                        )
-                     AND dm.domain_name IN('%(dom_string)s')
-                     """ %locals() ,params )
-    return acts
 
 def retrieve_acts_psql(domains, params):
     """Run a query for act_id, tid, component_id, compd_id and domain_name.
@@ -173,23 +139,6 @@ def write_table(lkp, flag_lkp, manuals, params, path):
 
 
 
-def upload_sql(params):
-    """ Load SQL table using connection string defined in global parameters.
-
-    Input:
-    params -- dictionary holding details of the connection string.
-
-    """
-    status = os.system("cp data/automatic_pfam_maps_v_%(version)s.tab data/pfam_maps.txt" % params)
-    if status != 0:
-        sys.exit("Error copying data/pfam_maps_v_%(version)s.tab to data/pfam_maps.txt" % params)
-    status = os.system("mysql -u%(user)s -p%(pword)s -h%(host)s -P%(port)s -e 'DROP TABLE %(release)s.pfam_maps'" % params)
-    status = os.system("mysql -u%(user)s -p%(pword)s -h%(host)s -P%(port)s -e 'CREATE TABLE pfam_maps(map_id INT NOT NULL AUTO_INCREMENT, activity_id INT, compd_id INT, domain_name VARCHAR(100), category_flag INT, status_flag INT, manual_flag INT, comment VARCHAR(150), timestamp VARCHAR(25),  PRIMARY KEY (map_id))' %(release)s"% params)
-    if status != 0:
-        sys.exit("Error creating table pfam_maps." % params)
-    os.system("mysqlimport -u%(user)s -p%(pword)s -h%(host)s -P%(port)s --ignore-lines=1 --lines-terminated-by='\n' --local %(release)s data/pfam_maps.txt" % params)
-    if status != 0:
-        sys.exit("Error loading table pfam_maps.""" % params)
 
 def upload_psql(params):
     """ Load SQL table using connection string defined in global parameters.
