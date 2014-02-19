2980096e4fa37179a1ab6740054585275f9aad4d
fak <fkrueger@ebi.ac.uk>
2014-02-19 14:33:04 +0000

    no more parameterless queries

--- pgQuery.py
+++ pgQuery.py
@@ -7,22 +7,11 @@ momo.sander@googlemail.com
 """
 import psycopg2
 
-def query(query, usr, pwd, db, hostname):
-    """
-    Processes a query without paramters.
-    """
-    conn = psycopg2.connect(host = params['host'], user = params['user'], password = params['pword'], database = params['release'], port = params['port'])
-    curs = conn.cursor()
-    curs.execute(query)
-    return curs.fetchall()
-
-
 def paramquery(query, param, params):
     """
     Processes a query with parameters.
     """
     conn = psycopg2.connect(host = params['host'], user = params['user'], password = params['pword'], database = params['release'], port = params['port'])
-
     curs = conn.cursor()
     curs.execute(query, param)
     return curs.fetchall()
